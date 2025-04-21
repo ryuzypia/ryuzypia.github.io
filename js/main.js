29c70a073587ffb4eb6e32825f14c9e48ca846f6
류정원 <ryuzy@naver.com>
2025-04-21 12:28:19 +0900

    대대적인 수정

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const header = document.querySelector(".header");
+
+const headerHeight = header.offsetHeight; // 요소의 총 높이
+// console.log(headerHeight); // 70
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > headerHeight) {
+    // console.log("window.scrollY가 headerHeight보다 큽니다.");
+    header.classList.add("header--dark");
+  } else {
+    // console.log("window.scrollY가 headerHeight보다 작습니다.");
+    header.classList.remove("header--dark");
+  }
+});
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
+
+arrowUp.addEventListener("click", (e) => {
+  e.preventDefault(); // a태그의 기본 동작을 막음
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+const navbarMenu = document.querySelector(".header__menu");
