165c0abd1f7fa5290e1d280aa99edaa771a69ba5
류정원 <ryuzy@naver.com>
2025-04-21 14:36:15 +0900

    포트폴리오 완성

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,6 @@
 const categories = document.querySelector(".categories");
 const projectsContainer = document.querySelector(".projects");
-const projects = document.querySelector(".project");
+const projects = document.querySelectorAll(".project");
 
 categories.addEventListener("click", (e) => {
   const filter = e.target.dataset.category;
@@ -10,10 +10,25 @@ categories.addEventListener("click", (e) => {
     return;
   }
   activeSelection(e.target);
+  filterProjects(filter);
 });
 
 function activeSelection(target) {
   const active = document.querySelector(".category--selected");
   active.classList.remove("category--selected");
   target.classList.add("category--selected");
+}
+
+function filterProjects(filter) {
+  projects.forEach((project) => {
+    if (filter == "all" || filter == project.dataset.type) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+  projectsContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 500);
 }
